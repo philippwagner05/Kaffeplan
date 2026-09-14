2eaf1ce3870246c8129a47067bb6417f672372c3
Philipp <philipp.wagner@setex-germany.com>
2026-09-14 12:23:35 +0200

    Add methods to Jahresplan for cleaning and filter exchange per employee; update Planeintrag properties

--- Kaffeeplan.Core/Model/Jahresplan.cs
+++ Kaffeeplan.Core/Model/Jahresplan.cs
@@ -1,8 +1,45 @@
-using System;
-
 namespace Kaffeeplan.Core.Model;
 
 public class Jahresplan
 {
+    public int Jahr { get; set; }
+    public List<Planeintrag> Eintraege { get; set; } = [];
+    public Dictionary<string, int> ReinigungProMitarbeiter()
+    {
+        Dictionary<string, int> reinigungProMitarbeiter = new Dictionary<string, int>();    
+        foreach (var eintrag in Eintraege)
+        {
+           if(reinigungProMitarbeiter.ContainsKey(eintrag.MitarbeiterName))
+            {
+                reinigungProMitarbeiter[eintrag.MitarbeiterName] += 1;
+            }
+            else
+            {
+                reinigungProMitarbeiter.Add(eintrag.MitarbeiterName, 1);
+            }
+        }
+        return reinigungProMitarbeiter;
+    }
 
+    public Dictionary<string, int> FiltertauschProMitarbeiter()
+    {
+        Dictionary<string, int> filtertauschProMitarbeiter = new Dictionary<string, int>();
+        foreach (var eintrag in Eintraege)
+        {
+            if((eintrag.Aufgabenart & Aufgabenart.Filtertausch) == Aufgabenart.Filtertausch)
+            {
+                if(filtertauschProMitarbeiter.ContainsKey(eintrag.MitarbeiterName))
+                {
+                    filtertauschProMitarbeiter[eintrag.MitarbeiterName] += 1;
+                }
+                else
+                {
+                    filtertauschProMitarbeiter.Add(eintrag.MitarbeiterName, 1);
+                }
+            }
+        }
+        return filtertauschProMitarbeiter;
+    }   
+    
+    
 }
--- Kaffeeplan.Core/Model/Planeintrag.cs
+++ Kaffeeplan.Core/Model/Planeintrag.cs
@@ -2,5 +2,10 @@ namespace Kaffeeplan.Core.Model;
 
 public class Planeintrag
 {
-
+    public int Kalenderwoche { get; set; }
+    public DateOnly MontagDatum { get; set; }
+    public string MitarbeiterName { get; set; } = string.Empty;
+    public Aufgabenart Aufgabenart { get; set; }
+    public Aufgabenart HatFiltertausch { get; set; } = Aufgabenart.Keine;
+    
 }
