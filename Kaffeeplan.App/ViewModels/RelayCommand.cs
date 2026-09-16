01a24969cbeb429580871cf171785815ae4a4fcb
Philipp <philipp.wagner@setex-germany.com>
2026-09-16 09:30:37 +0200

    Hinzufügen von ViewModelBasis und RelayCommand

---
+++ Kaffeeplan.App/ViewModels/RelayCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Kaffeeplan.App.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _ausfuehren;
+        private readonly Func<bool>? _kannAusfuehren;
+
+        public RelayCommand(Action ausfuhren, Func<bool>? kannAusfuehren = null)
+        {
+            _ausfuehren = ausfuhren ?? throw new ArgumentNullException(nameof(ausfuhren));
+            _kannAusfuehren = kannAusfuehren;
+        }
+
+        public bool CanExecute(object? parameter) => _kannAusfuehren?.Invoke() ?? true;
+
+        public void Execute(object? parameter) => _ausfuehren();
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add     => CommandManager.RequerySuggested += value;
+            remove  => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
---
+++ Kaffeeplan.App/ViewModels/ViewModelBasis.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Kaffeeplan.App.ViewModels
+{
+    public abstract class ViewModelBasis : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void MeldeAenderung([CallerMemberName] string? eigenschaft = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(eigenschaft));
+
+        protected bool SetzeWert<T>(ref T feld, T wert, [CallerMemberName] string? eigenschaft = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(feld, wert))
+                return false;
+
+            feld = wert;
+            MeldeAenderung(eigenschaft);
+            return true;
+        }
+    }
+}
